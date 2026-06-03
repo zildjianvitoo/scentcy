19effbe1c6936d30fb145623feb8022dea32ce2b
zildjianvitoo <zildjianvito07@gmail.com>
2026-06-03 21:17:12 +0700

    Initial Commit

---
+++ scentcy/scentcyApp.swift
@@ -0,0 +1,17 @@
+//
+//  scentcyApp.swift
+//  scentcy
+//
+//  Created by Zildjian Vito  on 03/06/26.
+//
+
+import SwiftUI
+
+@main
+struct scentcyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
