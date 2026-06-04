bb0588203f5d01babf404247ef26e8496d6d12d8
deedlacks <deedlacks@gmail.com>
2026-06-04 23:11:29 +0700

    following the stucture by vito

---
+++ scentcy/Features/Camera/Views/OnboardingHow.swift
@@ -0,0 +1,77 @@
+//
+//  OnboardingHow.swift
+//  scentcy
+//
+//  Created by Afra on 04/06/26.
+//
+
+import SwiftUI
+
+struct OnboardingHow: View {
+    var body: some View {
+        VStack(spacing: 40) {
+
+
+            VStack(spacing: 40) {
+                HStack {
+                    Image(.illustOnboard)
+                        .resizable()
+                        .frame(width: 316, height: 357)
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(24)
+                }
+               
+                VStack(alignment: .leading) {
+                    Text("Share Perfumes")
+                        .font(.title.bold())
+                    HStack{
+                        Text("You")
+                            .font(.title.bold())
+                        Text("enjoy")
+                            .font(.title.bold())
+                            .italic()
+                            .foregroundColor(.highlitedText)
+                        
+                    }
+                   
+                    Text("Upload perfumes you've smelled and liked. We'll learn your scent preferences from there.")
+                        .font(.callout)
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 316, height: 60, alignment: .topLeading)
+                    Spacer()
+                        
+                    NavigationLink(destination: OnboardingHowResult()) {
+                        Text("Next")
+                            .frame(width: 303, height: 48)
+                    }
+                    .tint(Color.black)
+                    .buttonStyle(.borderedProminent)
+                }
+                .frame(width: 316, alignment: .leading)
+                
+            }
+
+    
+        }
+        .padding(20)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.appBackground)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Skip") {
+                    
+                }
+            }
+            ToolbarItem (placement: .principal) {
+                OnboardingProgressDots(currentPage: 2)
+            }
+        }
+        
+        
+    }
+       
+}
+
+#Preview {
+    OnboardingHow()
+}
